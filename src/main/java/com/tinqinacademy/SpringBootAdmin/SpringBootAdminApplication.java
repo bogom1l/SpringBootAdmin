1c7b4df7a03737659064bc64ff7b46e24f338001
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-05 11:08:46 +0300

    Configured dependencies

--- src/main/java/com/tinqinacademy/SpringBootAdmin/SpringBootAdminApplication.java
+++ src/main/java/com/tinqinacademy/SpringBootAdmin/SpringBootAdminApplication.java
@@ -1,9 +1,11 @@
 package com.tinqinacademy.SpringBootAdmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
